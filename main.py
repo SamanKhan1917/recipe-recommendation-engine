87035ecf2ff805deaadb9d45b3f0943b641ebfe0
SamanKhan1917 <111567300+SamanKhan1917@users.noreply.github.com>
2024-09-23 21:17:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from fastapi
+from fastapi import FastAPI
+from . import routes
+
+app = FastAPI()
+
+app.include_router(routes.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Recipe Recommendation System!"}
